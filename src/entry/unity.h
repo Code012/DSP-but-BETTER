277ca6ae409b6c3053aa7960fd8f0d5e12ab6c2e
Shahbaz Bokhari <syedshahbazhaider007@gmail.com>
2025-11-03 05:15:37 +0000

    For clangd. Will be removed after development.

---
+++ src/entry/unity.h
@@ -0,0 +1,16 @@
+#ifndef UNITY_H
+#define UNITY_H
+
+//////////////////////
+// Headers
+#include "tester/simpletest.h"
+#include "base_inc.hpp"
+// #include "parse_inc.hpp"
+
+//////////////////////
+// Implementations
+#include "tester/simpletest.cpp"
+#include "base_inc.cpp"
+// #include "parse_inc.cpp"
+
+#endif // UNITY_H
