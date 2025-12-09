23c63b356ef89f0c4013ee86e0c4f93b2ca7203c
Shahbaz Bokhari <syedshahbazhaider007@gmail.com>
2025-12-09 03:16:55 +0000

    Clay + Raylib

--- build.bat
+++ build.bat
@@ -1,9 +1,10 @@
+:: TODO(me): this bash script needs some rewriting
 :: TODO(me): ADD A COMMAND FOR ADDRESS SANITISATION, LOOK AT RADDBG
 @echo off
 setlocal
 :: Parse command line arguments
-if "%1"=="test" set test=1
 if "%1"=="main" set main=1
+if "%1"=="test" set test=1
 if "%1"=="help" goto :help
 if "%1"=="-h" goto :help
 if "%1"=="?" goto :help
@@ -11,8 +12,10 @@ if "%1"=="?" goto :help
 if not exist build mkdir build
 
 :: Compiler flags with include paths
-set cl_common=/I../src /nologo /FC /Zi /EHsc /std:c++20
+set cl_common=/I../src /nologo /FC /Zi /EHsc /std:c++20 /MD 
+set link=/MACHINE:X64 /LIBPATH:../src/third_party/raylib/lib raylib.lib opengl32.lib kernel32.lib user32.lib shell32.lib gdi32.lib winmm.lib msvcrt.lib 
 set compile=call cl %cl_common%
+
 
 :: Build targets
 if "%test%"=="1" (
@@ -30,7 +33,7 @@ if "%test%"=="1" (
 )
 pushd build
 echo Compiling: %target%
-%compile% %target% /Fe:%output%
+%compile% %target% /Fe:%output% /link %link%
 
 if %ERRORLEVEL% neq 0 (
     echo Build failed!
