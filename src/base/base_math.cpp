1b5e311e35744ed9b895e5e65338b07fd83e08e9
Shahbaz Bokhari <syedshahbazhaider007@gmail.com>
2026-02-13 22:57:36 +0000

    Avoiding potential UB in C++ using unions, a shame I liked the union

--- src/base/base_math.cpp
+++ src/base/base_math.cpp
@@ -5,7 +5,7 @@
 
 //////////////////////////////
 // Range Ops
+internal U64 Dim1U64(Rng1U64 r)                         {U64 c = ((r.max() > r.min()) ? (r.max() - r.min()) : 0); return c;}
 
-internal U64 Dim1U64(Rng1U64 r)                         {U64 c = ((r.max > r.min) ? (r.max - r.min) : 0); return c;}
-
-internal U64 Dim1S64(Rng1U64 r)                         {S64 c = ((r.max > r.min) ? (r.max - r.min) : 0); return c;}
+internal S64 Dim1S64(Rng1S64 r)                         {S64 c = ((r.max() > r.min()) ? (r.max() - r.min()) : 0); return c;}
+internal S64 DeltaS64(Rng1S64 r)						{return r.max() - r.min();}
--- src/base/base_math.h
+++ src/base/base_math.h
@@ -7,25 +7,42 @@
 // Range Types
 
 // 1-range
-
-union Rng1U64
+// originally a union of min,max and v[2] but theres some nasty UB involving it so I had to change it
+struct Rng1U64
 {
-    struct
-    {
-        U64 min;
-        U64 max;
-    };
     U64 v[2];
+
+    Rng1U64() = default;
+    Rng1U64(U64 min, U64 max) : v{min, max}
+    {
+        if (min > max)
+        {
+            v[0] = min;
+            v[1] = max;
+        }
+    }
+
+    U64& min()  { return v[0]; }
+    U64& max()  { return v[1]; }
 };
 
-union Rng1S64
+struct Rng1S64
 {
-    struct
-    {
-        S64 min;
-        S64 max;
-    };
     S64 v[2];
+
+    Rng1S64() = default;
+    Rng1S64(S64 min, S64 max) : v{min, max}
+    {
+        if (min > max)
+        {
+            v[0] = max;
+            v[1] = min;
+        }
+    }
+
+
+    S64& min() { return v[0]; }
+    S64& max() { return v[1]; }
 };
 
 ////////////////////////////////
@@ -34,5 +51,6 @@ union Rng1S64
 internal U64 Dim1U64(Rng1U64 r);
 
 internal S64 Dim1S64(Rng1S64 r);
+internal S64 DeltaS64(Rng1S64 r);
 
 #endif // BASE_MATH_HPP
